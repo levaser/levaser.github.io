7899d7a6a9e8b4bc467b1f6e3c0f2398ec4f12ec
levaser <vovus402@gmail.com>
2023-10-21 09:51:37 +0300

    update

--- task5/main.js
+++ task5/main.js
@@ -1,10 +1,26 @@
 function calculateCost() {
-    const product = parseFloat(document.getElementById('product').value);
+    const cost = parseInt(document.getElementById('cost').value);
     const quantity = parseInt(document.getElementById('quantity').value);
 
-    document.getElementById('cost').innerHTML = `Стоимость: ${product * quantity} руб.`;
+    document.getElementById('result').innerHTML = `Стоимость: ${cost * quantity} руб.`;
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('calculate').addEventListener('click', calculateCost);
+function validateInput() {
+    const prodCost = document.getElementById('quantity').value;
+    const regex = /^[0-9]+$/;
+
+    if (!regex.test(prodCost)) {
+        document.getElementById('result').textContent = 'Некорректное значение';
+        return false;
+    } else {
+        return true;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('calculate').addEventListener('click', function() {
+        if (validateInput()) {
+            calculateCost();
+        }
+    });
 });
